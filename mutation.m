490723cefd5daa69671b566f5127c7723623e070
Dikshit Chauhan <112845953+ChauhanDikshit@users.noreply.github.com>
2024-12-12 13:12:16 +0800

    Update mutation.m

--- mutation.m
+++ mutation.m
@@ -1,12 +1,6 @@
-% function [population,centroid]=mutation(population,charge,count,iter,change,range,alpha,beta,rho,phi)
+%% For non-uniform mutation please cite the refer paper: Kar, Devroop, et al. "Fuzzy mutation embedded hybrids of gravitational search and Particle Swarm Optimization methods for engineering design problems."
+%% Engineering Applications of Artificial Intelligence 95 (2020): 103847.
 function [population,centroid]=mutation(population,charge,count,iter,change,range,alpha,beta,gbest)
-% % mass=massCalculation(rank);
-% mass=charge+.00005;
-% centroid=zeros(1,size(population,2));
-% for i=1:size(population,2)
-%     centroid(1,i)=(mass*population(:,i))/sum(mass);
-% end
-
 [~,s]=sort(charge);
 for i=1:size(population,1)
     for j = 1:size(population,1)
@@ -15,27 +9,16 @@ for i=1:size(population,1)
     distance(find(distance==0))=[];
     [Novel_val, Novel_IX] = sort(distance,'ascend');
     dist = sum(Novel_val(Novel_IX(1:5)));
-%     k=randperm(size(population,1));
-%     if k~=i
-%     dist=norm(population(k,:)-population(i,:));
-%     else
-%      dist=norm(population(s(i),:)-population(i,:));  
-%     end
     pm=1/(dist+1);
     pc=.5+.5*(tanh((change/alpha)-beta));
-    %pm=min(pc,pm);
-    pm = 0.8*pm + 0.2*pc ;
-%     pm = rho*pm + phi*pc ;
     for j=1:size(population,2)
         if ((rand < pm) || (pc > .98))
-            %fprintf('l');
+
             if abs(population(i,j)) <= 0.00001
-                delta=.5*range*rand;%ranje(j)
+                delta=.5*range*rand;
             else
                 delta=min(.5*range*((iter-count)/iter)^20,abs(population(i,j)));
             end
-            %delta=max(delta,population(i,j)/10); %bad results
-            %delta=.5*range*((1-(count/iter))^2);
             if rand <.5
                 population(i,:)=population(i,:)-rand*delta;
             else
@@ -43,6 +26,4 @@ for i=1:size(population,1)
             end
         end
     end
-    
-%     end
 end
